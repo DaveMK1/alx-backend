f048c78c5323ceacb84b9a06c180ad10350f7ae2
David Agbesinyale <davidm7040@gmail.com>
2024-07-23 21:26:31 +0000

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -7,19 +7,19 @@ from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """
-    FIFOCache implements a First-In-First-Out caching strategy
+    FIFOCache defines a FIFO caching system
     """
 
     def __init__(self):
         """
-        Initialize the class using the parent's constructor method
+        Initialize the class with the parent's init method
         """
         super().__init__()
         self.usage = []
 
     def put(self, key, item):
         """
-        Store a key-value pair in the cache
+        Cache a key-value pair
         """
         if key is None or item is None:
             pass
@@ -36,10 +36,11 @@ class MRUCache(BaseCaching):
 
     def get(self, key):
         """
-        Fetch the value associated with a given key, or return None
+        Return the value linked to a given key, or None
         """
         if key is not None and key in self.cache_data.keys():
             del self.usage[self.usage.index(key)]
             self.usage.append(key)
             return self.cache_data[key]
         return None
+

295d60229f1f2c0ba059c3a19715baab0e819b1d
David Agbesinyale <davidm7040@gmail.com>
2024-07-30 14:07:51 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,17 @@
+#!/usr/bin/env python3
 """
-Flask app setup
+A Basic flask application
 """
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """
-    Renders a basic html template
-    """
+@app.route("/")
+def home():
+    """ display home route"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
